e8c9089b338aa41bd7e730794b3963bb501db82f
Leon <Liangz.org@gmail.com>
2020-10-21 22:52:30 +0800

    feat: complete game data structure

--- config.py
+++ config.py
@@ -100,7 +100,7 @@ class Config:
             }
         },
         'Garden gnome': {
-            'price': 15.9,
+            'price': 15.90,
             'part': {
                 'Concrete': 1,
                 'Paint': 1,
@@ -125,4 +125,186 @@ class Config:
             }
         },
 
+        'Air gun': {
+            'price': 19.78,
+            'part': {
+                'Metal': 4,
+                'Hose': 2
+            }
+        },
+        'Arm': {
+            'price': 51.68,
+            'part': {
+                'Metal': 2,
+                'Circuit': 1,
+                'Motor': 2
+            }
+        },
+        'Conveyor': {
+            'price': 102.58,
+            'part': {
+                'Belt': 1,
+                'Gear': 2,
+                'Metal wheel': 8,
+                'Motor': 1
+            }
+        },
+        'Lifter': {
+            'price': 55.46,
+            'part': {
+                'Metal': 3,
+                'Cable': 2,
+                'Hose': 2,
+                'Motor': 1,
+                'Pump': 1
+            }
+        },
+        'Logic unit': {
+            'price': 26.75,
+            'part': {
+                'Wire': 5,
+                'Circuit': 4
+            }
+        },
+        'Mover': {
+            'price': 113.65,
+            'part': {
+                'Metal': 3,
+                'Gear': 4,
+                'Metal wheel': 4,
+                'Motor': 2
+            }
+        },
+        'Road': {
+            'price': 49.80,
+            'part': {
+                'Concrete': 4,
+                'Adv concrete': 2
+            }
+        },
+        'Support': {
+            'price': 40.53,
+            'part': {
+                'Metal': 2,
+                'Adv concrete': 2
+            }
+        },
+        'Thing-a-ma-jig': {
+            'price': 61.02,
+            'part': {
+                'Circuit': 3,
+                'Hose': 2,
+                'Motor': 1,
+                'Pump': 2
+            }
+        },
+        'Widget': {
+            'price': 14.78,
+            'part': {
+                'Metal': 1,
+                'Plastic': 4,
+                'Wire': 2,
+                'Circuit': 1
+            }
+        },
+        'Toy car': {
+            'price': 35.38,
+            'part': {
+                'Metal': 1,
+                'Paint': 1,
+                'Plastic': 3,
+                'Plastic wheel': 4,
+                'Box': 1
+            }
+        },
+        'Water gun': {
+            'price': 29.62,
+            'part': {
+                'Paint': 1,
+                'Plastic': 6,
+                'Hose': 2,
+                'Box': 1
+            }
+        },
+        'Adv logic unit': {
+            'price': 87.15,
+            'part': {
+                'Wire': 4,
+                'Circuit': 4,
+                'Logic unit': 2
+            }
+        },
+        'Assembly line': {
+            'price': 715.84,
+            'part': {
+                'Air gun': 2,
+                'Arm': 2,
+                'Conveyor': 3,
+                'Lifter': 1,
+                'Mover': 1
+            }
+        },
+        'Jet engine': {
+            'price': 180.04,
+            'part': {
+                'Metal': 8,
+                'Wire': 12,
+                'Hose': 6,
+                'Pump': 4,
+                'Thing-a-ma-jig': 1
+            }
+        },
+        'Sensor': {
+            'price': 51.51,
+            'part': {
+                'Rare metal': 2,
+                'Wire': 1,
+                'Circuit': 1,
+                'Logic unit': 1
+            }
+        },
+        'Bridge': {
+            'price': 1091.16,
+            'part': {
+                'Road': 6,
+                'Support': 6
+            }
+        },
+        'Forklift': {
+            'price': 198.04,
+            'part': {
+                'Metal': 6,
+                'Rubber': 8,
+                'Metal wheel': 4,
+                'Motor': 2,
+                'Box': 4
+            }
+        },
+        'Radio tower': {
+            'price': 414.00,
+            'part': {
+                'Metal': 12,
+                'Wire': 6,
+                'Support': 4
+            }
+        },
+        'Tablet computer': {
+            'price': 93.14,
+            'part': {
+                'Plastic': 1,
+                'Wire': 3,
+                'Circuit': 3,
+                'Logic unit': 1,
+                'Box': 1
+            }
+        },
+        'Builder:Tier 1': {
+            'price': 328.74,
+            'part': {
+                'Motor': 1,
+                'Arm': 2,
+                'Conveyor': 1,
+                'Thing-a-ma-jig': 1
+            }
+        }
     }
